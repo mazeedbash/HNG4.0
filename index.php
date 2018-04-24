a330e510a36e25c6bc8f494bb58662f7204925fe
mazeedbash <bashorun.mazeed@gmail.com>
2018-04-24 11:24:29 +0100

    first push

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HNG 4.0</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+		<center>
+			<h1>HNG 4.0 INTERN</h1>
+		</center>
+		<div id="page">
+			<center>
+			
+			<?php 
+				$date = date('Y/m/d H:i:s');
+				echo "<h2>By Bashorun <br>@<br>$date </h2>";
+			 ?>
+			 </center>
+		</div>
+</body>
+</html>
